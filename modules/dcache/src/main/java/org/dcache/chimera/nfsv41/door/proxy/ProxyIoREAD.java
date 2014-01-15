kofemann/dcache

commit 93ec46a3a1be602c0e47d54abd8b0cb12ebb730f

nfs4: implement proxy-io on read
Linux client too often falls back to IO through MDS (door).
This ends up with IO error on the client side as dCache does
not supports it.

This change set aims to add proxy IO by using internally DCAP
protocol. The door-pool communication can be changed later to
other protocol.

This is for master, nevertheless, 2.7 and 2.6 should have it as well.

Acked-by: Gerd Behrmann
Target: master
Require-book: no
Require-notes: no

modules/dcache/src/main/java/org/dcache/chimera/nfsv41/door/proxy/ProxyIoREAD.java
@@ -1,0 +1,125 @@
+package org.dcache.chimera.nfsv41.door.proxy;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import diskCacheV111.util.CacheException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+
+import org.dcache.nfs.ChimeraNFSException;
+import org.dcache.nfs.nfsstat;
+import org.dcache.nfs.v4.AbstractNFSv4Operation;
+import org.dcache.nfs.v4.CompoundContext;
+import org.dcache.nfs.v4.NFS4State;
+import org.dcache.nfs.v4.StateDisposeListener;
+import org.dcache.nfs.v4.xdr.READ4res;
+import org.dcache.nfs.v4.xdr.READ4resok;
+import org.dcache.nfs.v4.xdr.nfs_argop4;
+import org.dcache.nfs.v4.xdr.nfs_opnum4;
+import org.dcache.nfs.v4.xdr.nfs_resop4;
+import org.dcache.nfs.v4.xdr.stateid4;
+import org.dcache.nfs.vfs.Inode;
+import org.dcache.xdr.RpcCall;
+
+public class ProxyIoREAD extends AbstractNFSv4Operation {
+
+    private static final Logger _log = LoggerFactory.getLogger(ProxyIoREAD.class.getName());
+    private final DcapProxyIoFactory proxyIoFactory;
+
+    public ProxyIoREAD(nfs_argop4 args, DcapProxyIoFactory proxyIoFactory) {
+        super(args, nfs_opnum4.OP_READ);
+        this.proxyIoFactory = proxyIoFactory;
+    }
+
+    @Override
+    public void process(CompoundContext context, nfs_resop4 result) {
+        final READ4res res = result.opread;
+
+        try {
+
+            long offset = _args.opread.offset.value.value;
+            int count = _args.opread.count.value.value;
+            stateid4 stateid = _args.opread.stateid;
+
+            ProxyIoAdapter proxyIoAdapter = getOrCreateProxy(context.currentInode(), stateid, context);
+            ByteBuffer bb = ByteBuffer.allocate(count);
+            int bytesReaded = proxyIoAdapter.read(bb, offset);
+
+            res.status = nfsstat.NFS_OK;
+            res.resok4 = new READ4resok();
+            res.resok4.data = bb;
+
+            if( offset + bytesReaded == proxyIoAdapter.size() ) {
+                res.resok4.eof = true;
+            }
+
+            _log.debug("MOVER: {}@{} readed, {} requested.", bytesReaded, offset, _args.opread.count.value.value);
+
+        }catch(ChimeraNFSException he) {
+            res.status = he.getStatus();
+            _log.debug(he.getMessage());
+        }catch(IOException ioe) {
+            _log.error("DSREAD: ", ioe);
+            res.status = nfsstat.NFSERR_IO;
+        }catch(Exception e) {
+            _log.error("DSREAD: ", e);
+            res.status = nfsstat.NFSERR_SERVERFAULT;
+        }
+    }
+
+    private ProxyIoAdapter getOrCreateProxy(final Inode inode, final stateid4 stateid, final CompoundContext context) throws ChimeraNFSException {
+
+        try {
+            ProxyIoAdapter adapter = _prioxyIO.get(stateid,
+                    new Callable<ProxyIoAdapter>() {
+
+                        @Override
+                        public ProxyIoAdapter call() throws Exception {
+                            final RpcCall call = context.getRpcCall();
+                            final ProxyIoAdapter adapter = proxyIoFactory.getAdapter(inode, call.getCredential().getSubject(),
+                                    call.getTransport().getRemoteSocketAddress());
+
+
+                            final NFS4State state = context.getStateHandler().getClientIdByStateId(stateid).state(stateid);
+                            state.addDisposeListener( new StateDisposeListener() {
+
+                                @Override
+                                public void notifyDisposed(NFS4State state) {
+                                    try {
+                                        adapter.close();
+                                    }catch (IOException e) {
+                                        _log.error("failed fo close io adapter: ", e.getMessage());
+                                    }
+                                    _prioxyIO.invalidate(state.stateid());
+                                }
+                            });
+
+                            return adapter;
+                        }
+                    });
+
+            return adapter;
+        } catch (ExecutionException e) {
+            Throwable t = e.getCause();
+            _log.error("failed to create IO adapter: ", t.getMessage());
+            if (t instanceof ChimeraNFSException) {
+                throw (ChimeraNFSException)t;
+            }
+            int status = nfsstat.NFSERR_IO;
+            if (t instanceof CacheException ) {
+                status = nfsstat.NFSERR_DELAY;
+            }
+            throw new ChimeraNFSException(status, t.getMessage());
+        }
+    }
+
+    private static final Cache<stateid4, ProxyIoAdapter> _prioxyIO=
+            CacheBuilder.newBuilder()
+            .build();
+
+}

modules/dcache/src/main/java/org/dcache/chimera/nfsv41/door/NFSv41Door.java
@@ -3,6 +3,8 @@
  */
 package org.dcache.chimera.nfsv41.door;
 
+import org.dcache.chimera.nfsv41.door.proxy.DcapProxyIoFactory;
+import org.dcache.chimera.nfsv41.door.proxy.ProxyIoMdsOpFactory;
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
@@ -61,7 +63,6 @@ import org.dcache.nfs.v3.xdr.mount_prot;
 import org.dcache.nfs.v3.xdr.nfs3_prot;
 import org.dcache.nfs.v4.CompoundContext;
 import org.dcache.nfs.v4.Layout;
-import org.dcache.nfs.v4.MDSOperationFactory;
 import org.dcache.nfs.v4.NFS4Client;
 import org.dcache.nfs.v4.NFSServerV41;
 import org.dcache.nfs.v4.NFSv41DeviceManager;
@@ -86,6 +87,7 @@ import org.dcache.chimera.nfsv41.mover.NFS4ProtocolInfo;
 import org.dcache.commons.stats.RequestExecutionTimeGauges;
 import org.dcache.commons.util.NDC;
 import dmg.cells.services.login.LoginBrokerHandler;
+import org.dcache.nfs.v4.MDSOperationFactory;
 import org.dcache.util.RedirectedTransfer;
 import org.dcache.util.Transfer;
 import org.dcache.util.TransferRetryPolicy;
@@ -173,6 +175,8 @@ public class NFSv41Door extends AbstractCellComponent implements
 
     private LoginBrokerHandler _loginBrokerHandler;
 
+    private DcapProxyIoFactory _proxyIoFactory;
+
     private final static TransferRetryPolicy RETRY_POLICY =
         new TransferRetryPolicy(Integer.MAX_VALUE, NFS_RETRY_PERIOD,
                                 NFS_REPLY_TIMEOUT, NFS_REPLY_TIMEOUT);
@@ -254,7 +258,15 @@ public class NFSv41Door extends AbstractCellComponent implements
                     break;
                 case V41:
                      final NFSv41DeviceManager _dm = this;
-                    _nfs4 = new NFSServerV41(new MDSOperationFactory(),
+                     _proxyIoFactory = new DcapProxyIoFactory(getCellAddress().getCellName() + "-dcap-proxy", "");
+                     _proxyIoFactory.setBillingStub(_billingStub);
+                     _proxyIoFactory.setFileSystemProvider(_fileFileSystemProvider);
+                     _proxyIoFactory.setPnfsHandler(_pnfsHandler);
+                     _proxyIoFactory.setPoolManager(_poolManagerStub.getDestinationPath());
+                     _proxyIoFactory.setIoQueue(_ioQueue);
+                     _proxyIoFactory.setRetryPolicy(RETRY_POLICY);
+                     _proxyIoFactory.startAdapter();
+                    _nfs4 = new NFSServerV41(new ProxyIoMdsOpFactory(_proxyIoFactory, new MDSOperationFactory()),
                             _dm, _vfs, _idMapper, _exportFile);
                     _rpcService.register(new OncRpcProgram(nfs4_prot.NFS4_PROGRAM, nfs4_prot.NFS_V4), _nfs4);
                     _loginBrokerHandler.start();
@@ -272,6 +284,9 @@ public class NFSv41Door extends AbstractCellComponent implements
     public void destroy() throws IOException {
         _loginBrokerHandler.stop();
         _rpcService.stop();
+        if(_proxyIoFactory != null) {
+            _proxyIoFactory.cleanUp();
+        }
     }
 
     /*
@@ -302,7 +317,6 @@ public class NFSv41Door extends AbstractCellComponent implements
         }
 
         stateid4 stateid = message.challange();
-
         NfsTransfer transfer = _ioMessages.get(stateid);
         transfer.redirect(device);
     }
@@ -313,8 +327,8 @@ public class NFSv41Door extends AbstractCellComponent implements
         _log.debug("Mover {} done.", protocolInfo.stateId());
         Transfer transfer = _ioMessages.remove(protocolInfo.stateId());
         if(transfer != null) {
-            transfer.finished(transferFinishedMessage);
-            transfer.notifyBilling(transferFinishedMessage.getReturnCode(), "");
+                transfer.finished(transferFinishedMessage);
+                transfer.notifyBilling(transferFinishedMessage.getReturnCode(), "");
         }
     }
 

modules/dcache/src/main/java/org/dcache/chimera/nfsv41/door/proxy/DcapChannelImpl.java
@@ -1,0 +1,170 @@
+package org.dcache.chimera.nfsv41.door.proxy;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.SocketChannel;
+
+/**
+ * DCAP based implementation of {@link ProxyIoAdapter}.
+ */
+public class DcapChannelImpl implements ProxyIoAdapter {
+
+    /**
+     * DCAP binary commands
+     */
+    private static final int CLOSE = 4;
+    private static final int SEEK_AND_READ = 11;
+    private static final int SEEK_AND_WRITE = 12;
+
+    /**
+     * DCAP SEEK whence
+     */
+    private static final int SEEK_SET = 0;
+
+    private final SocketChannel _channel;
+    private final long _size;
+
+    public DcapChannelImpl(InetSocketAddress addr, int session, byte[] challange, long size) throws IOException {
+        _channel = SocketChannel.open(addr);
+        _channel.configureBlocking(true);
+        ByteBuffer buf = ByteBuffer.allocate(8 + challange.length);
+        buf.order(ByteOrder.BIG_ENDIAN);
+        buf.putInt(session);
+        buf.putInt(challange.length).put(challange);
+        buf.flip();
+        writeFully(_channel, buf);
+        _size = size;
+    }
+
+    @Override
+    public synchronized int read(ByteBuffer dst, long position) throws IOException {
+
+        ByteBuffer command = new DcapCommandBuilder()
+                .withSeekAndRead(position, dst.remaining())
+                .build();
+
+        writeFully(_channel, command);
+        getAck();
+        return getData(dst);
+    }
+
+    @Override
+    public synchronized int write(ByteBuffer src, long position) throws IOException {
+        throw new IOException("write is not allowed");
+    }
+
+    @Override
+    public long size() {
+        return _size;
+    }
+
+    @Override
+    public synchronized void close() throws IOException {
+        ByteBuffer command = new DcapCommandBuilder()
+                .withClose()
+                .build();
+
+        writeFully(_channel, command);
+        getAck();
+        _channel.close();
+    }
+
+    private int getData(ByteBuffer buf) throws IOException {
+        ByteBuffer dataBlock = ByteBuffer.allocate(128);
+        dataBlock.order(ByteOrder.BIG_ENDIAN);
+
+        dataBlock.limit(8);
+        readFully(_channel, dataBlock);
+        int total = 0;
+        while (true) {
+            dataBlock.clear();
+            dataBlock.limit(4);
+            readFully(_channel, dataBlock);
+            dataBlock.flip();
+            int n = dataBlock.getInt();
+            if (n < 0) {
+                getAck();
+                break;
+            }
+
+            ByteBuffer chunk = buf.slice();
+            chunk.limit(n);
+            readFully(_channel, chunk);
+            buf.position(buf.position() + n);
+            total += n;
+        }
+        return total;
+    }
+
+    private void getAck() throws IOException {
+        ByteBuffer ackBuffer = ByteBuffer.allocate(256);
+        ackBuffer.order(ByteOrder.BIG_ENDIAN);
+        ackBuffer.limit(4);
+        readFully(_channel, ackBuffer);
+        ackBuffer.flip();
+        int len = ackBuffer.getInt();
+        ackBuffer.clear().limit(len);
+        readFully(_channel, ackBuffer);
+        // FIXME: error handling
+    }
+
+
+    private static void writeFully(SocketChannel channel, ByteBuffer buf) throws IOException {
+        while (buf.hasRemaining()) {
+            channel.write(buf);
+        }
+    }
+
+    private static void readFully(SocketChannel channel, ByteBuffer buf) throws IOException {
+        while (buf.hasRemaining()) {
+            if (channel.read(buf) < 0) {
+                throw new EOFException("EOF on input socket (fillBuffer)");
+            }
+        }
+    }
+
+    private static class DcapCommandBuilder {
+
+        private final ByteBuffer _command;
+
+        DcapCommandBuilder() {
+            _command = ByteBuffer.allocate(8192);
+            _command.order(ByteOrder.BIG_ENDIAN);
+            _command.position(Integer.SIZE / 8);
+        }
+
+        DcapCommandBuilder withSeekAndRead(long offset, long len) {
+            _command.putInt(SEEK_AND_READ);
+            _command.putLong(offset);
+            _command.putInt(SEEK_SET);
+            _command.putLong(len);
+            return this;
+        }
+
+        DcapCommandBuilder withSeekAndWrite(long offset, int len) {
+            _command.putInt(SEEK_AND_WRITE);
+            _command.putLong(offset);
+            _command.putInt(SEEK_SET);
+            return this;
+        }
+
+        DcapCommandBuilder withClose() {
+            _command.putInt(CLOSE);
+            return this;
+        }
+
+        DcapCommandBuilder withByteCount(long count) {
+            _command.putLong(count);
+            return this;
+        }
+
+        ByteBuffer build() {
+            _command.putInt(0, _command.position() - 4);
+            _command.flip();
+            return _command;
+        }
+    }
+}

modules/dcache/src/main/java/org/dcache/chimera/nfsv41/door/proxy/DcapProxyIoFactory.java
@@ -1,0 +1,136 @@
+package org.dcache.chimera.nfsv41.door.proxy;
+
+import com.google.common.base.Charsets;
+import diskCacheV111.util.Base64;
+import diskCacheV111.util.CacheException;
+import diskCacheV111.util.FsPath;
+import diskCacheV111.util.PnfsHandler;
+import diskCacheV111.util.PnfsId;
+import diskCacheV111.vehicles.DCapProtocolInfo;
+import diskCacheV111.vehicles.DoorTransferFinishedMessage;
+import diskCacheV111.vehicles.PoolPassiveIoFileMessage;
+import dmg.cells.nucleus.CellAddressCore;
+import dmg.cells.nucleus.CellPath;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicInteger;
+import javax.security.auth.Subject;
+import org.dcache.auth.Subjects;
+import org.dcache.cells.AbstractCell;
+import org.dcache.cells.CellStub;
+import org.dcache.chimera.JdbcFs;
+import org.dcache.nfs.vfs.Inode;
+import org.dcache.util.RedirectedTransfer;
+import org.dcache.util.TransferRetryPolicy;
+import org.jboss.netty.util.internal.ConcurrentHashMap;
+
+/**
+ *
+ */
+public class DcapProxyIoFactory extends AbstractCell {
+
+    private PnfsHandler _pnfsHandler;
+    private CellStub _poolManagerStub;
+    private CellStub _billingStub;
+    private String _ioQueue;
+    /**
+     * Given that the timeout is pretty short, the retry period has to be rather
+     * small too.
+     */
+    private final static long NFS_RETRY_PERIOD = 500; // In millis
+    private final AtomicInteger _session = new AtomicInteger();
+
+    private final ConcurrentHashMap<Integer, DcapTransfer> _pendingIO =
+            new ConcurrentHashMap<>();
+
+    private TransferRetryPolicy _retryPolicy;
+
+    private JdbcFs _fileFileSystemProvider;
+
+    public void setFileSystemProvider(JdbcFs fs) {
+        _fileFileSystemProvider = fs;
+    }
+
+    public void setPnfsHandler(PnfsHandler pnfs) {
+        _pnfsHandler = pnfs;
+    }
+
+    public void setBillingStub(CellStub stub) {
+        _billingStub = stub;
+    }
+
+    public void setIoQueue(String queue) {
+        _ioQueue = queue;
+    }
+
+    public void setRetryPolicy(TransferRetryPolicy retryPolicy) {
+        _retryPolicy = retryPolicy;
+    }
+
+    public void setPoolManager(CellPath poolManager) {
+        _poolManagerStub = new CellStub(this, poolManager);
+    }
+
+    public DcapProxyIoFactory(String cellName, String arguments) {
+        super(cellName, arguments);
+    }
+
+    ProxyIoAdapter getAdapter(Inode inode, Subject subject, InetSocketAddress client) throws CacheException, InterruptedException, IOException {
+
+        final int session = nextSession();
+        final DCapProtocolInfo protocolInfo = new DCapProtocolInfo("DCap", 3, 0, client);
+        protocolInfo.door( new CellPath(new CellAddressCore(getCellName(), getCellDomainName())));
+        protocolInfo.setSessionId(session);
+        protocolInfo.isPassive(true);
+
+        final PnfsId  pnfsId = new PnfsId(_fileFileSystemProvider.inodeFromBytes(inode.getFileId()).toString());
+        final DcapTransfer transfer = new DcapTransfer(_pnfsHandler, subject);
+        DcapTransfer.initSession();
+        transfer.setProtocolInfo(protocolInfo);
+        transfer.setCellName(getCellName());
+        transfer.setDomainName(getCellDomainName());
+        transfer.setBillingStub(_billingStub);
+        transfer.setPoolStub(_poolManagerStub);
+        transfer.setPoolManagerStub(_poolManagerStub);
+        transfer.setPnfsId(pnfsId);
+        transfer.setClientAddress(client);
+        transfer.readNameSpaceEntry();
+
+        _pendingIO.put(session, transfer);
+        transfer.selectPoolAndStartMover(_ioQueue, _retryPolicy);
+        PoolPassiveIoFileMessage<byte[]> redirect = transfer.waitForRedirect(NFS_RETRY_PERIOD);
+
+        return new DcapChannelImpl(redirect.socketAddress(), session,
+                Base64.byteArrayToBase64(redirect.challange()).getBytes(Charsets.US_ASCII),
+                transfer.getFileAttributes().getSize());
+    }
+
+    public void startAdapter() throws InterruptedException, ExecutionException {
+        doInit();
+    }
+
+    public void messageArrived(PoolPassiveIoFileMessage<byte[]> message) {
+
+        final int session = (int)message.getId();
+        final DcapTransfer transfer = _pendingIO.remove(session);
+        if (transfer != null) {
+            transfer.redirect(message);
+        }
+    }
+
+    public void messageArrived(DoorTransferFinishedMessage transferFinishedMessage) {
+        // nop
+    }
+
+    private int nextSession() {
+        return _session.getAndIncrement();
+    }
+
+    private static class DcapTransfer extends RedirectedTransfer<PoolPassiveIoFileMessage<byte[]>> {
+
+        public DcapTransfer(PnfsHandler pnfs, Subject ioSubject) {
+            super(pnfs, Subjects.ROOT, ioSubject, new FsPath("/"));
+        }
+    }
+}

modules/dcache/src/main/java/org/dcache/chimera/nfsv41/door/proxy/ProxyIoAdapter.java
@@ -1,0 +1,42 @@
+package org.dcache.chimera.nfsv41.door.proxy;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+/**
+ *
+ */
+public interface ProxyIoAdapter extends Closeable {
+
+    /**
+     * Reads a sequence of bytes from this channel into the given buffer,
+     * starting at the given file position.
+     *
+     * @param dst The buffer into which bytes are to be transferred
+     * @param position The file position at which the transfer is to begin; must
+     * be non-negative
+     * @return The number of bytes read, possibly zero, or -1 if the given
+     * position is greater than or equal to the file's current size
+     * @throws IOException
+     */
+    int read(ByteBuffer dst, long position) throws IOException;
+
+    /**
+     * Writes a sequence of bytes to this channel from the given buffer,
+     * starting at the given file position.
+     *
+     * @param src The buffer from which bytes are to be transferred
+     * @param position The file position at which the transfer is to begin; must
+     * be non-negative
+     * @return The number of bytes written, possibly zero
+     * @throws IOException
+     */
+    int write(ByteBuffer src, long position) throws IOException;
+
+    /**
+     * Returns the size of the file access by this adapter.
+     * @return size of the files,  measured in bytes
+     */
+    long size();
+}

modules/dcache/src/main/java/org/dcache/chimera/nfsv41/door/proxy/ProxyIoMdsOpFactory.java
@@ -1,0 +1,31 @@
+package org.dcache.chimera.nfsv41.door.proxy;
+
+import org.dcache.nfs.v4.AbstractNFSv4Operation;
+import org.dcache.nfs.v4.NFSv4OperationFactory;
+import org.dcache.nfs.v4.xdr.nfs_argop4;
+import org.dcache.nfs.v4.xdr.nfs_opnum4;
+
+/**
+ * NFS operation factory which uses Proxy IO adapter for read requests
+ */
+public class ProxyIoMdsOpFactory implements NFSv4OperationFactory {
+
+    private final DcapProxyIoFactory _proxyIoFactory;
+    private final NFSv4OperationFactory _inner;
+
+    public ProxyIoMdsOpFactory(DcapProxyIoFactory proxyIoFactory, NFSv4OperationFactory inner) {
+        _proxyIoFactory = proxyIoFactory;
+        _inner = inner;
+    }
+
+    @Override
+    public AbstractNFSv4Operation getOperation(nfs_argop4 op) {
+        switch (op.argop) {
+            case nfs_opnum4.OP_READ:
+                return new ProxyIoREAD(op, _proxyIoFactory);
+            default:
+                return _inner.getOperation(op);
+        }
+    }
+
+}

commit 4bb578dad468ca7ed250a3b6a9e690f33e42b6ad

nfs4: fix IO on special files
regression introduced in 93ec46a3a1be602c0e47d54abd8b0cb12ebb730f

Acked-by:
Target: master, 2.7
Require-book: no
Require-notes: no

modules/dcache/src/main/java/org/dcache/chimera/nfsv41/door/proxy/ProxyIoREAD.java
@@ -16,6 +16,7 @@ import org.dcache.nfs.nfsstat;
 import org.dcache.nfs.v4.AbstractNFSv4Operation;
 import org.dcache.nfs.v4.CompoundContext;
 import org.dcache.nfs.v4.NFS4State;
+import org.dcache.nfs.v4.OperationREAD;
 import org.dcache.nfs.v4.StateDisposeListener;
 import org.dcache.nfs.v4.xdr.READ4res;
 import org.dcache.nfs.v4.xdr.READ4resok;
@@ -42,11 +43,20 @@ public class ProxyIoREAD extends AbstractNFSv4Operation {
 
         try {
 
+            Inode inode = context.currentInode();
+            if (!context.getFs().hasIOLayout(inode)) {
+                /*
+                 * if we have a special file, then fall back to regular read operation
+                 */
+                new OperationREAD(_args).process(context, result);
+                return;
+            }
+
             long offset = _args.opread.offset.value.value;
             int count = _args.opread.count.value.value;
             stateid4 stateid = _args.opread.stateid;
 
-            ProxyIoAdapter proxyIoAdapter = getOrCreateProxy(context.currentInode(), stateid, context);
+            ProxyIoAdapter proxyIoAdapter = getOrCreateProxy(inode, stateid, context);
             ByteBuffer bb = ByteBuffer.allocate(count);
             int bytesReaded = proxyIoAdapter.read(bb, offset);
 