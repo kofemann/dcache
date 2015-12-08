kofemann/dcache

commit fa9a749c47f3fdb22421ebb2f48d927d9c6f15da

chimera: Read AL and RP on directory listing
Motivation:

In 2.14 we embedded the access latency and retention policy in t_inodes and
thus in the Stat object. We fail to populate the access latency and retention
policy in the Stat object during directory listing.

This oversight is a direct consequence of code duplication in Chimera.

Modification:

Refactor the t_inodes to Stat mapping such that it can be reused between the
code doing listing and the implementation of the stat call.

Result:

Correct access latency and retention policy in directory listings (propably mostly
something WebDAV and SRM care about).

Target: trunk
Request: 2.14
Require-notes: yes
Require-book: no
Acked-by: Paul Millar <paul.millar@desy.de>
Acked-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
Patch: https://rb.dcache.org/r/8844/

modules/chimera/src/main/java/org/dcache/chimera/FsSqlDriver.java
@@ -40,6 +40,7 @@ import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
@@ -47,8 +48,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.EnumSet;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -179,7 +182,59 @@ class FsSqlDriver {
      * @return
      */
     DirectoryStreamB<HimeraDirectoryEntry> newDirectoryStream(FsInode dir) {
-        return new DirectoryStreamImpl(dir, _jdbc);
+        return new DirectoryStreamB<HimeraDirectoryEntry>()
+        {
+            final DirectoryStreamImpl stream = new DirectoryStreamImpl(dir, _jdbc);
+
+            @Override
+            public Iterator<HimeraDirectoryEntry> iterator()
+            {
+                return new Iterator<HimeraDirectoryEntry>()
+                {
+                    private HimeraDirectoryEntry current = innerNext();
+
+                    @Override
+                    public boolean hasNext()
+                    {
+                        return current != null;
+                    }
+
+                    @Override
+                    public HimeraDirectoryEntry next()
+                    {
+                        if (current == null) {
+                            throw new NoSuchElementException("No more entries");
+                        }
+                        HimeraDirectoryEntry entry = current;
+                        current = innerNext();
+                        return entry;
+                    }
+
+                    protected HimeraDirectoryEntry innerNext()
+                    {
+                        try {
+                            ResultSet rs = stream.next();
+                            if (rs == null) {
+                                return null;
+                            }
+                            Stat stat = toStat(rs);
+                            FsInode inode = new FsInode(dir.getFs(), rs.getString("ipnfsid"), FsInodeType.INODE, 0, stat);
+                            inode.setParent(dir);
+                            return new HimeraDirectoryEntry(rs.getString("iname"), inode, stat);
+                        } catch (SQLException e) {
+                            _log.error("failed to fetch next entry: {}", e.getMessage());
+                            return null;
+                        }
+                    }
+                };
+            }
+
+            @Override
+            public void close() throws IOException
+            {
+                stream.close();
+            }
+        };
     }
 
     /**
@@ -271,53 +326,67 @@ class FsSqlDriver {
     }
 
     public Stat stat(FsInode inode, int level) {
-        String sql;
+        Stat stat;
         if (level == 0) {
-            sql = "SELECT isize,inlink,itype,imode,iuid,igid,iatime,ictime,imtime,icrtime,igeneration,iaccess_latency,iretention_policy FROM t_inodes WHERE ipnfsid=?";
+            stat = _jdbc.query(
+                    "SELECT * FROM t_inodes WHERE ipnfsid = ?",
+                    ps -> ps.setString(1, inode.toString()),
+                    rs -> rs.next() ? toStat(rs) : null);
         } else {
-            sql = "SELECT isize,inlink,imode,iuid,igid,iatime,ictime,imtime FROM t_level_" + level + " WHERE ipnfsid=?";
+            stat = _jdbc.query(
+                    "SELECT * FROM t_level_" + level + " WHERE ipnfsid = ?",
+                    ps -> ps.setString(1, inode.toString()),
+                    rs -> rs.next() ? toStatLevel(rs) : null);
         }
+        if (stat != null) {
+            stat.setIno((int) inode.id());
+        }
+        return stat;
+    }
 
-        return _jdbc.query(sql,
-                           ps -> ps.setString(1, inode.toString()),
-                           rs -> {
-                               if (!rs.next()) {
-                                   return null;
-                               }
-                               Stat stat = new Stat();
-                               int inodeType;
+    private Stat toStat(ResultSet rs) throws SQLException
+    {
+        Stat stat = new Stat();
+        int inodeType = rs.getInt("itype");
+        stat.setCrTime(rs.getTimestamp("icrtime").getTime());
+        stat.setGeneration(rs.getLong("igeneration"));
+        int rp = rs.getInt("iretention_policy");
+        if (!rs.wasNull()) {
+            stat.setRetentionPolicy(RetentionPolicy.getRetentionPolicy(rp));
+        }
+        int al = rs.getInt("iaccess_latency");
+        if (!rs.wasNull()) {
+            stat.setAccessLatency(AccessLatency.getAccessLatency(al));
+        }
+        stat.setSize(rs.getLong("isize"));
+        stat.setATime(rs.getTimestamp("iatime").getTime());
+        stat.setCTime(rs.getTimestamp("ictime").getTime());
+        stat.setMTime(rs.getTimestamp("imtime").getTime());
+        stat.setUid(rs.getInt("iuid"));
+        stat.setGid(rs.getInt("igid"));
+        stat.setMode(rs.getInt("imode") | inodeType);
+        stat.setNlink(rs.getInt("inlink"));
+        stat.setDev(17);
+        stat.setRdev(13);
+        return stat;
+    }
 
-                               if (level == 0) {
-                                   inodeType = rs.getInt("itype");
-                                   stat.setCrTime(rs.getTimestamp("icrtime").getTime());
-                                   stat.setGeneration(rs.getLong("igeneration"));
-                                   int rp = rs.getInt("iretention_policy");
-                                   if (!rs.wasNull()) {
-                                       stat.setRetentionPolicy(RetentionPolicy.getRetentionPolicy(rp));
-                                   }
-                                   int al = rs.getInt("iaccess_latency");
-                                   if (!rs.wasNull()) {
-                                       stat.setAccessLatency(AccessLatency.getAccessLatency(al));
-                                   }
-                               } else {
-                                   inodeType = UnixPermission.S_IFREG;
-                                   stat.setCrTime(rs.getTimestamp("imtime").getTime());
-                                   stat.setGeneration(0);
-                               }
-
-                               stat.setSize(rs.getLong("isize"));
-                               stat.setATime(rs.getTimestamp("iatime").getTime());
-                               stat.setCTime(rs.getTimestamp("ictime").getTime());
-                               stat.setMTime(rs.getTimestamp("imtime").getTime());
-                               stat.setUid(rs.getInt("iuid"));
-                               stat.setGid(rs.getInt("igid"));
-                               stat.setMode(rs.getInt("imode") | inodeType);
-                               stat.setNlink(rs.getInt("inlink"));
-                               stat.setIno((int) inode.id());
-                               stat.setDev(17);
-                               stat.setRdev(13);
-                               return stat;
-                           });
+    private Stat toStatLevel(ResultSet rs) throws SQLException
+    {
+        Stat stat = new Stat();
+        stat.setCrTime(rs.getTimestamp("imtime").getTime());
+        stat.setGeneration(0);
+        stat.setSize(rs.getLong("isize"));
+        stat.setATime(rs.getTimestamp("iatime").getTime());
+        stat.setCTime(rs.getTimestamp("ictime").getTime());
+        stat.setMTime(rs.getTimestamp("imtime").getTime());
+        stat.setUid(rs.getInt("iuid"));
+        stat.setGid(rs.getInt("igid"));
+        stat.setMode(rs.getInt("imode") | UnixPermission.S_IFREG);
+        stat.setNlink(rs.getInt("inlink"));
+        stat.setDev(17);
+        stat.setRdev(13);
+        return stat;
     }
 
     /**

modules/chimera/src/main/java/org/dcache/chimera/DirectoryStreamImpl.java
@@ -16,8 +16,6 @@
  */
 package org.dcache.chimera;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceUtils;
@@ -28,39 +26,23 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
 
-import org.dcache.chimera.posix.Stat;
-
-public class DirectoryStreamImpl implements DirectoryStreamB<HimeraDirectoryEntry>,
-        Iterator<HimeraDirectoryEntry> {
-
-    private static final Logger _log = LoggerFactory.getLogger(DirectoryStreamImpl.class);
-
+public class DirectoryStreamImpl
+{
     private static final String QUERY =
-            "SELECT i.ipnfsid, d.iname, i.isize, i.inlink, i.imode, i.itype, " +
-            "i.iuid, i.igid, i.iatime, i.ictime, i.imtime, i.icrtime, i.igeneration " +
-            "FROM t_inodes i JOIN t_dirs d ON i.ipnfsid = d.ipnfsid WHERE iparent=? " +
+            "SELECT i.*, d.iname FROM t_inodes i JOIN t_dirs d ON i.ipnfsid = d.ipnfsid WHERE iparent=? " +
             "UNION ALL " +
-            "SELECT i.ipnfsid, '.', i.isize, i.inlink, i.imode, i.itype, " +
-            "i.iuid, i.igid, i.iatime, i.ictime, i.imtime, i.icrtime, i.igeneration " +
-            "FROM t_inodes i WHERE i.ipnfsid=? " +
+            "SELECT i.*, '.' FROM t_inodes i WHERE i.ipnfsid=? " +
             "UNION ALL " +
-            "SELECT i.ipnfsid, '..', i.isize, i.inlink, i.imode, i.itype, " +
-            "i.iuid, i.igid, i.iatime, i.ictime, i.imtime, i.icrtime, i.igeneration " +
-            "FROM t_inodes i JOIN t_dirs d ON i.ipnfsid = d.iparent WHERE d.ipnfsid=?";
+            "SELECT i.*, '..' FROM t_inodes i JOIN t_dirs d ON i.ipnfsid = d.iparent WHERE d.ipnfsid=?";
 
     private final ResultSet _resultSet;
-    private final FsInode _dir;
     private final JdbcTemplate _jdbc;
     private final Connection _connection;
     private final PreparedStatement _statement;
-    private boolean _hasPendingElement;
 
-    DirectoryStreamImpl(FsInode dir, JdbcTemplate jdbc) {
-        _dir = dir;
-        _hasPendingElement = false;
+    DirectoryStreamImpl(FsInode dir, JdbcTemplate jdbc)
+    {
         _jdbc = jdbc;
 
         Connection connection = null;
@@ -84,7 +66,6 @@ public class DirectoryStreamImpl implements DirectoryStreamB<HimeraDirectoryEntr
         _statement = ps;
     }
 
-    @Override
     public void close() throws IOException
     {
         try {
@@ -96,67 +77,8 @@ public class DirectoryStreamImpl implements DirectoryStreamB<HimeraDirectoryEntr
         }
     }
 
-    @Override
-    public Iterator<HimeraDirectoryEntry> iterator() {
-        return this;
-    }
-
-    @Override
-    public boolean hasNext() {
-
-        /*
-         * To be complient with the semantics: calling hasNext() several times
-         * shold not move cursor.
-         *
-         * Cache the result of the last call as long as next() was't called.
-         */
-        if (_hasPendingElement) {
-            return true;
-        }
-        try {
-            boolean hasNext = _resultSet.next();
-            if (hasNext) {
-                _hasPendingElement = true;
-            }
-            return hasNext;
-        } catch (SQLException ex) {
-            _log.error("failed check for next entry: " + ex.getMessage());
-            return false;
-        }
-    }
-
-    @Override
-    public HimeraDirectoryEntry next() {
-
-        try {
-            Stat stat = new Stat();
-            stat.setSize(_resultSet.getLong("isize"));
-            stat.setATime(_resultSet.getTimestamp("iatime").getTime());
-            stat.setCTime(_resultSet.getTimestamp("ictime").getTime());
-            stat.setMTime(_resultSet.getTimestamp("imtime").getTime());
-            stat.setCrTime(_resultSet.getTimestamp("icrtime").getTime());
-            stat.setUid(_resultSet.getInt("iuid"));
-            stat.setGid(_resultSet.getInt("igid"));
-            stat.setMode(_resultSet.getInt("imode") | _resultSet.getInt("itype"));
-            stat.setNlink(_resultSet.getInt("inlink"));
-            stat.setGeneration(_resultSet.getInt("igeneration"));
-            FsInode inode = new FsInode(_dir.getFs(), _resultSet.getString("ipnfsid"));
-            inode.setParent(_dir);
-            stat.setIno((int) inode.id());
-            stat.setDev(17);
-            stat.setRdev(13);
-
-            inode.setStatCache(stat);
-            _hasPendingElement = false;
-            return new HimeraDirectoryEntry(_resultSet.getString("iname"), inode, stat);
-        } catch (SQLException e) {
-            _log.error("failed to fetch next entry: " + e.getMessage());
-            throw new NoSuchElementException("Got SQL exception: " + e.getMessage());
-        }
-    }
-
-    @Override
-    public void remove() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public ResultSet next() throws SQLException
+    {
+        return _resultSet.next() ? _resultSet : null;
     }
 }

commit 71332c7da587f8567cf158d07eb193cfead30146

chimera: fix regressin introduced by fa9a749c4
Motivation:
the file's ino field is not initialized and produced stacktrace:

java.lang.IllegalStateException: Attribute is not defined: INO
	at org.dcache.chimera.posix.Stat.guard(Stat.java:353) ~[chimera-2.15.0-SNAPSHOT.jar:2.15.0-SNAPSHOT]
	at org.dcache.chimera.posix.Stat.getIno(Stat.java:103) ~[chimera-2.15.0-SNAPSHOT.jar:2.15.0-SNAPSHOT]
	at org.dcache.chimera.nfsv41.door.ChimeraVfs.fromChimeraStat(ChimeraVfs.java:316) ~[dcache-nfs-2.15.0-SNAPSHOT.jar:2.15.0-SNAPSHOT]
	at org.dcache.chimera.nfsv41.door.ChimeraVfs.access$200(ChimeraVfs.java:70) ~[dcache-nfs-2.15.0-SNAPSHOT.jar:2.15.0-SNAPSHOT]
	at org.dcache.chimera.nfsv41.door.ChimeraVfs$ChimeraDirectoryEntryToVfs.apply(ChimeraVfs.java:412) ~[dcache-nfs-2.15.0-SNAPSHOT.jar:2.15.0-SNAPSHOT]
	at org.dcache.chimera.nfsv41.door.ChimeraVfs$ChimeraDirectoryEntryToVfs.apply(ChimeraVfs.java:408) ~[dcache-nfs-2.15.0-SNAPSHOT.jar:2.15.0-SNAPSHOT]
	at com.google.common.collect.Lists$TransformingRandomAccessList.get(Lists.java:608) ~[guava-18.0.jar:na]
	at com.google.common.collect.Lists$TransformingRandomAccessList.get(Lists.java:608) ~[guava-18.0.jar:na]
	at org.dcache.nfs.v4.OperationREADDIR.process(OperationREADDIR.java:220) ~[nfs4j-core-0.11.2.jar:0.11.2]
	at org.dcache.nfs.v4.NFSServerV41.NFSPROC4_COMPOUND_4(NFSServerV41.java:152) ~[nfs4j-core-0.11.2.jar:0.11.2]
	at org.dcache.nfs.v4.xdr.nfs4_prot_NFS4_PROGRAM_ServerStub.dispatchOncRpcCall(nfs4_prot_NFS4_PROGRAM_ServerStub.java:48) [nfs4j-core-0.11.2.jar:0.11.2]
	at org.dcache.xdr.RpcDispatcher$1.run(RpcDispatcher.java:82) [oncrpc4j-core-2.4.2.jar:na]
	at org.glassfish.grizzly.threadpool.AbstractThreadPool$Worker.doWork(AbstractThreadPool.java:591) [grizzly-framework-2.3.23.jar:2.3.23]
	at org.glassfish.grizzly.threadpool.AbstractThreadPool$Worker.run(AbstractThreadPool.java:571) [grizzly-framework-2.3.23.jar:2.3.23]
	at java.lang.Thread.run(Thread.java:745) [na:1.8.0_31]

Modification:
Add missing field

Result:

modules/chimera/src/main/java/org/dcache/chimera/FsSqlDriver.java
@@ -220,6 +220,7 @@ class FsSqlDriver {
                             Stat stat = toStat(rs);
                             FsInode inode = new FsInode(dir.getFs(), rs.getString("ipnfsid"), FsInodeType.INODE, 0, stat);
                             inode.setParent(dir);
+                            stat.setIno((int)inode.id());
                             return new HimeraDirectoryEntry(rs.getString("iname"), inode, stat);
                         } catch (SQLException e) {
                             _log.error("failed to fetch next entry: {}", e.getMessage());