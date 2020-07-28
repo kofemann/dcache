70ca3d604ab5a868b79fc978043f3e4a59581d8b
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2020-07-28 09:29:42 +0200

    [squashme] srr with storage units as share

--- modules/dcache-frontend/src/main/java/org/dcache/restful/resources/srr/record/SrrBuilder.java
+++ modules/dcache-frontend/src/main/java/org/dcache/restful/resources/srr/record/SrrBuilder.java
@@ -1,10 +1,14 @@
 package org.dcache.restful.resources.srr.record;
 
+import com.google.common.base.Splitter;
+import com.google.common.util.concurrent.ListenableFuture;
+import diskCacheV111.poolManager.CostModule;
 import diskCacheV111.poolManager.PoolPreferenceLevel;
 import diskCacheV111.poolManager.PoolSelectionUnit;
 import diskCacheV111.services.space.Space;
 import diskCacheV111.services.space.message.GetSpaceTokensMessage;
 import diskCacheV111.util.CacheException;
+import dmg.cells.nucleus.CellPath;
 import dmg.cells.nucleus.NoRouteToCellException;
 import dmg.cells.services.login.LoginBrokerSubscriber;
 import org.dcache.cells.CellStub;
@@ -17,7 +21,9 @@ import java.io.File;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.concurrent.ExecutionException;
 import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -176,10 +182,18 @@ public class SrrBuilder {
 
         PnfsTopDirectoriesMessage topDirectoriesMessage = pnfsmanager.sendAndWait(new PnfsTopDirectoriesMessage());
 
-        List<Storageshare> storageshares = new ArrayList<>();
+        Map<String, Storageshare> storageshares = new HashMap<>();
         long now = Instant.now().getEpochSecond();
 
         for (Map.Entry<String, FileAttributes> d : topDirectoriesMessage.getTopDirs().entrySet()) {
+
+            FileAttributes attribute = d.getValue();
+            String storageUnit = attribute.getStorageClass() + "@" + attribute.getHsm();
+            String shareName = attribute.getStorageClass().toUpperCase().replace(':', '-');
+
+            if (storageshares.containsKey(shareName)) {
+                continue;
+            }
 
             PoolPreferenceLevel[] l = remotePoolMonitor.getPoolSelectionUnit().match(
                     PoolSelectionUnit.DirectionType.WRITE,
@@ -193,37 +207,64 @@ public class SrrBuilder {
                 continue;
             }
 
+            CostModule costModule = remotePoolMonitor.getCostModule();
             long totalSpace = 0;
             long usedSpace = 0;
+
             for (PoolPreferenceLevel sl : l) {
                 List<String> pools = sl.getPoolList();
 
                 totalSpace += pools.stream()
-                        .map(p -> remotePoolMonitor.getCostModule().getPoolCostInfo(p))
+                        .map(costModule::getPoolCostInfo)
                         .mapToLong(p -> p.getSpaceInfo().getTotalSpace())
                         .sum();
 
                 usedSpace += pools.stream()
-                        .map(p -> remotePoolMonitor.getCostModule().getPoolCostInfo(p))
+                        .map(costModule::getPoolCostInfo)
                         .mapToLong(p -> p.getSpaceInfo().getTotalSpace() - p.getSpaceInfo().getFreeSpace() - p.getSpaceInfo().getRemovableSpace())
                         .sum();
+
+                usedSpace = pools.stream()
+                        .mapToLong(p -> {
+                            ListenableFuture<String> reply = pnfsmanager.send(new CellPath(p), "rep ls -s", String.class);
+                            try {
+                                String out = reply.get();
+                                List<String> stat = Splitter.on('\n')
+                                        .omitEmptyStrings()
+                                        .trimResults()
+                                        .splitToList(out);
+
+                                return stat.stream()
+                                        .filter(o -> o.startsWith(storageUnit))
+                                        .map(o -> o.split("\\s+"))
+                                        .mapToLong(o -> Long.parseLong(o[3]) + Long.parseLong(o[5]))
+                                        .sum();
+                            } catch (InterruptedException e) {
+                                return 0;
+                            } catch(ExecutionException e) {
+                                System.out.println("Error: " + e.getCause().toString());
+                                return 0;
+                            }
+                        })
+                        .sum();
+
             }
 
             String vos = group2vo.getOrDefault(Integer.toString(d.getValue().getGroup()), "gid:" + d.getValue().getGroup());
 
+
             Storageshare share = new Storageshare()
-                    .withName(d.getKey().equals("/") ? "ROOT" : new File(d.getKey()).getName().toUpperCase())
+                    .withName(shareName)
                     .withTotalsize(totalSpace)
                     .withUsedsize(usedSpace)
                     .withTimestamp(now)
                     .withVos(Collections.singletonList(vos))
                     .withAssignedendpoints(Collections.singletonList("all"))
-                    .withPath(Collections.singletonList(d.getKey()))
                     .withAccesslatency(Storageshare.Accesslatency.fromValue(d.getValue().getAccessLatency()))
                     .withRetentionpolicy(Storageshare.Retentionpolicy.fromValue(d.getValue().getRetentionPolicy()));
-            storageshares.add(share);
+            storageshares.put(shareName, share);
         }
-        return storageshares;
+        return new ArrayList<>(storageshares.values());
     }
 
     private List<Storageendpoint> collectEndpoint() {
