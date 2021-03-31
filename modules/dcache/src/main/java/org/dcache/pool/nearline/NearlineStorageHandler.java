kofemann/dcache

commit 4759b3fe2ee5ca4eb8b5cd16e1be0f2dc21a4fe0

pool: don't scan flush/restore/remove queue to get stats
Motivation:
There are several cases when pool scans the various hsm queus:
  - on mover start, to check load assumptions
  - on heartbeat, to populate pool manager with load information
  - on 'info' command, to report load numbers to admin interface of
    dcache-view collectors

As this is O(n) operation, in case of a large queues those scans
might take seconds, which is unacceptable, especially wen starting
a new mover.

Modification:
Introduce QueueStat object to keep track of the current state. Use
CAS mechanism to avoid unnecessary locking. Update corresponding
getActive/getQueued methods to use the QueueStat object.

Result:
getting hsm start is O(1) and doesn't impact pools operation.

Acked-by: Svenja Meyer
Acked-by: Paul Millar
Target: master, 7.0, 6.2
Require-book: no
Require-notes: yes

modules/dcache/src/main/java/org/dcache/pool/nearline/NearlineStorageHandler.java
@@ -63,8 +63,10 @@ import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+import javax.annotation.Nonnull;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.annotation.concurrent.Immutable;
 
 import java.util.OptionalLong;
 
@@ -134,6 +136,43 @@ public class NearlineStorageHandler
         implements CellCommandListener, StateChangeListener, CellSetupProvider, CellLifeCycleAware, CellInfoProvider, CellIdentityAware,
                 PoolDataBeanProvider<StorageHandlerData>
 {
+
+    /**
+     * Queue occupation statistics.
+     *
+     * REVISIT: change to record type when java14 is allowed.
+     */
+    @Immutable
+    public static final class QueueStat {
+
+        private final int queued;
+        private final int active;
+        private final int canceled;
+
+        public QueueStat(int queued, int active, int canceled) {
+
+            checkArgument(queued >= 0);
+            checkArgument(active >= 0);
+            checkArgument(canceled >= 0);
+
+            this.queued = queued;
+            this.active = active;
+            this.canceled = canceled;
+        }
+
+        public int queued() {
+            return queued;
+        }
+
+        public int active() {
+            return active;
+        }
+
+        public int canceled() {
+            return canceled;
+        }
+    }
+
     private static final Logger LOGGER = LoggerFactory.getLogger(NearlineStorageHandler.class);
 
     private final FlushRequestContainer flushRequests = new FlushRequestContainer();
@@ -406,32 +445,35 @@ public class NearlineStorageHandler
 
     public int getActiveFetchJobs()
     {
-        return stageRequests.getCount(r -> (r.state.get() == AbstractRequest.State.ACTIVE || r.state.get() == AbstractRequest.State.CANCELED));
+        QueueStat queueStat = stageRequests.getQueueStats();
+        return queueStat.active() + queueStat.canceled();
     }
 
     public int getFetchQueueSize()
     {
-        return stageRequests.getCount(r -> r.state.get() == AbstractRequest.State.QUEUED);
+        return stageRequests.getQueueStats().queued();
     }
 
     public int getActiveStoreJobs()
     {
-        return flushRequests.getCount(r -> (r.state.get() == AbstractRequest.State.ACTIVE || r.state.get() == AbstractRequest.State.CANCELED));
+        QueueStat queueStat = flushRequests.getQueueStats();
+        return queueStat.active() + queueStat.canceled();
     }
 
     public int getStoreQueueSize()
     {
-        return flushRequests.getCount(r -> r.state.get() == AbstractRequest.State.QUEUED);
+        return flushRequests.getQueueStats().queued();
     }
 
     public int getActiveRemoveJobs()
     {
-        return removeRequests.getCount(r -> (r.state.get() == AbstractRequest.State.ACTIVE || r.state.get() == AbstractRequest.State.CANCELED));
+        QueueStat queueStat = removeRequests.getQueueStats();
+        return queueStat.active() + queueStat.canceled();
     }
 
     public int getRemoveQueueSize()
     {
-        return removeRequests.getCount(r -> r.state.get() == AbstractRequest.State.QUEUED);
+        return removeRequests.getQueueStats().queued();
     }
 
     @Override
@@ -478,9 +520,16 @@ public class NearlineStorageHandler
 
         private final List<Future<?>> asyncTasks = new ArrayList<>();
 
-        AbstractRequest(NearlineStorage storage)
+        /**
+         * Reference to queue statistics counter.
+         */
+        private final AtomicReference<QueueStat> queueStat;
+
+        AbstractRequest(NearlineStorage storage, @Nonnull AtomicReference<QueueStat> queueStat)
         {
             this.storage = storage;
+            this.queueStat = queueStat;
+            incQueued();
         }
 
         // Implements NearlineRequest#setIncluded
@@ -515,6 +564,7 @@ public class NearlineStorageHandler
                 return Futures.immediateFailedFuture(new IllegalStateException("Request is no longer queued."));
             }
             activatedAt = System.currentTimeMillis();
+            incActiveFromQueued();
             return Futures.immediateFuture(null);
         }
 
@@ -532,8 +582,14 @@ public class NearlineStorageHandler
 
         public void cancel()
         {
-            if (state.getAndSet(State.CANCELED) != State.CANCELED) {
+            State oldState = state.getAndSet(State.CANCELED);
+            if ( oldState != State.CANCELED) {
                 storage.cancel(uuid);
+                if (oldState == State.ACTIVE) {
+                    incCancelFromActive();
+                } else {
+                    incCancelFromQueued();
+                }
                 synchronized(this) {
                     for (Future<?> task : asyncTasks) {
                         /*
@@ -583,6 +639,87 @@ public class NearlineStorageHandler
         {
             return Long.compare(createdAt, o.createdAt);
         }
+
+        public void removeFromQueue() {
+            State currentState = state.get();
+            switch (currentState) {
+                case QUEUED:
+                    decQueued();
+                    break;
+                case ACTIVE:
+                    decActive();
+                    break;
+                case CANCELED:
+                    decCanceled();
+                    break;
+                default:
+                    throw new RuntimeException();
+            }
+        }
+
+        private void incQueued() {
+            boolean isSet;
+            do {
+                QueueStat current = queueStat.get();
+                QueueStat withThis = new QueueStat(current.queued() +1, current.active(), current.canceled());
+                isSet = queueStat.compareAndSet(current, withThis);
+            } while (!isSet);
+        }
+
+        private void incActiveFromQueued() {
+            boolean isSet;
+            do {
+                QueueStat current = queueStat.get();
+                QueueStat withThis = new QueueStat(current.queued() -1, current.active() + 1, current.canceled());
+                isSet = queueStat.compareAndSet(current, withThis);
+            } while (!isSet);
+        }
+
+        private void incCancelFromActive() {
+            boolean isSet;
+            do {
+                QueueStat current = queueStat.get();
+                QueueStat withThis = new QueueStat(current.queued(), current.active() - 1, current.canceled() + 1);
+                isSet = queueStat.compareAndSet(current, withThis);
+            } while (!isSet);
+        }
+
+        private void incCancelFromQueued() {
+            boolean isSet;
+            do {
+                QueueStat current = queueStat.get();
+                QueueStat withThis = new QueueStat(current.queued() - 1, current.active(), current.canceled() + 1);
+                isSet = queueStat.compareAndSet(current, withThis);
+            } while (!isSet);
+        }
+
+        private void decQueued() {
+            boolean isSet;
+            do {
+                QueueStat current = queueStat.get();
+                QueueStat withThis = new QueueStat(current.queued() -1, current.active(), current.canceled());
+                isSet = queueStat.compareAndSet(current, withThis);
+            } while (!isSet);
+        }
+
+        private void decActive() {
+            boolean isSet;
+            do {
+                QueueStat current = queueStat.get();
+                QueueStat withThis = new QueueStat(current.queued(), current.active() - 1, current.canceled());
+                isSet = queueStat.compareAndSet(current, withThis);
+            } while (!isSet);
+        }
+
+        private void decCanceled() {
+            boolean isSet;
+            do {
+                QueueStat current = queueStat.get();
+                QueueStat withThis = new QueueStat(current.queued(), current.active(), current.canceled() -1);
+                isSet = queueStat.compareAndSet(current, withThis);
+            } while (!isSet);
+        }
+
     }
 
     /**
@@ -604,6 +741,11 @@ public class NearlineStorageHandler
         protected final ConcurrentHashMap<K, R> requests = new ConcurrentHashMap<>();
 
         private final ContainerState state = new ContainerState();
+
+        /**
+         * Queue usage statistics.
+         */
+        protected AtomicReference<QueueStat> queueStats = new AtomicReference<>(new QueueStat(0, 0, 0));
 
         public void addAll(NearlineStorage storage,
                            Iterable<F> files,
@@ -697,16 +839,11 @@ public class NearlineStorageHandler
         }
 
         /**
-         * Return number of requests matching the given predicate.
-         * @param filter predicate to match.
-         * @return
+         * Get current request states statistics.
+         * @return request states statistics.
          */
-        public int getCount(Predicate<R> filter)
-        {
-            return (int)requests.values()
-                    .stream()
-                    .filter(filter)
-                    .count();
+        public QueueStat getQueueStats() {
+            return queueStats.get();
         }
 
         /**
@@ -743,6 +880,8 @@ public class NearlineStorageHandler
             if (actualRequest == null) {
                 return Collections.emptyList();
             }
+
+            actualRequest.removeFromQueue();
             state.decrement();
             return actualRequest.callbacks();
         }
@@ -769,7 +908,7 @@ public class NearlineStorageHandler
         protected FlushRequestImpl createRequest(NearlineStorage storage, PnfsId id)
                 throws CacheException, InterruptedException
         {
-            return new FlushRequestImpl(storage, id);
+            return new FlushRequestImpl(storage, queueStats, id);
         }
 
         @Override
@@ -792,7 +931,7 @@ public class NearlineStorageHandler
                                                  FileAttributes file)
                 throws CacheException
         {
-            return new StageRequestImpl(storage, file);
+            return new StageRequestImpl(storage, queueStats, file);
         }
 
         @Override
@@ -813,7 +952,7 @@ public class NearlineStorageHandler
         @Override
         protected RemoveRequestImpl createRequest(NearlineStorage storage, URI uri)
         {
-            return new RemoveRequestImpl(storage, uri);
+            return new RemoveRequestImpl(storage, queueStats, uri);
         }
 
         @Override
@@ -828,9 +967,9 @@ public class NearlineStorageHandler
         private final ReplicaDescriptor descriptor;
         private final StorageInfoMessage infoMsg;
 
-        public FlushRequestImpl(NearlineStorage nearlineStorage, PnfsId pnfsId) throws CacheException, InterruptedException
+        public FlushRequestImpl(NearlineStorage nearlineStorage, AtomicReference<QueueStat> stats, PnfsId pnfsId) throws CacheException, InterruptedException
         {
-            super(nearlineStorage);
+            super(nearlineStorage, stats);
             descriptor = repository.openEntry(pnfsId, EnumSet.of(OpenFlags.NOATIME));
             infoMsg = new StorageInfoMessage(cellAddress, pnfsId, false);
             infoMsg.setStorageInfo(descriptor.getFileAttributes().getStorageInfo());
@@ -1100,9 +1239,9 @@ public class NearlineStorageHandler
         private final ReplicaDescriptor descriptor;
         private ListenableFuture<Void> allocationFuture;
 
-        public StageRequestImpl(NearlineStorage storage, FileAttributes fileAttributes) throws CacheException
+        public StageRequestImpl(NearlineStorage storage, AtomicReference<QueueStat> stats, FileAttributes fileAttributes) throws CacheException
         {
-            super(storage);
+            super(storage, stats);
             PnfsId pnfsId = fileAttributes.getPnfsId();
             infoMsg = new StorageInfoMessage(cellAddress, pnfsId, true);
             infoMsg.setStorageInfo(fileAttributes.getStorageInfo());
@@ -1256,9 +1395,9 @@ public class NearlineStorageHandler
     {
         private final URI uri;
 
-        RemoveRequestImpl(NearlineStorage storage, URI uri)
+        RemoveRequestImpl(NearlineStorage storage, AtomicReference<QueueStat> stats, URI uri)
         {
-            super(storage);
+            super(storage, stats);
             this.uri = uri;
             LOGGER.debug("Remove request created for {}.", uri);
         }

commit 57b53ac103d128caeb7c6e2cac4ec49541e723e4

nearline-storage: fix store cancel regression
Fixes: 4759b3fe2e

Motivation:
we should update the number of canceled requests before
NearlineStorage#cancel is called as cancel will decrease the number of
canceled requests when the request is removed from the flush queue.

Caused by: java.lang.IllegalArgumentException: null
        at com.google.common.base.Preconditions.checkArgument(Preconditions.java:127)
        at org.dcache.pool.nearline.NearlineStorageHandler$QueueStat.<init>(NearlineStorageHandler.java:156)
        at org.dcache.pool.nearline.NearlineStorageHandler$AbstractRequest.decCanceled(NearlineStorageHandler.java:718)
        at org.dcache.pool.nearline.NearlineStorageHandler$AbstractRequest.removeFromQueue(NearlineStorageHandler.java:653)
        at org.dcache.pool.nearline.NearlineStorageHandler$AbstractRequestContainer.remove(NearlineStorageHandler.java:884)
        at org.dcache.pool.nearline.NearlineStorageHandler$AbstractRequestContainer.removeAndCallback(NearlineStorageHandler.java:858)
        at org.dcache.pool.nearline.NearlineStorageHandler$FlushRequestImpl.done(NearlineStorageHandler.java:1172)
        at org.dcache.pool.nearline.NearlineStorageHandler$FlushRequestImpl.failed(NearlineStorageHandler.java:1048)
        at org.dcache.SapphireDriver.lambda$cancel$1(SapphireDriver.java:121)
        at java.base/java.util.Optional.ifPresent(Optional.java:183)
        at org.dcache.SapphireDriver.cancel(SapphireDriver.java:118)
        at org.dcache.pool.nearline.NearlineStorageHandler$AbstractRequest.cancel(NearlineStorageHandler.java:587)
        at org.dcache.pool.nearline.NearlineStorageHandler$AbstractRequestContainer.cancel(NearlineStorageHandler.java:797)
        at org.dcache.pool.nearline.NearlineStorageHandler$StoreKillCommand.call(NearlineStorageHandler.java:1533)
        at org.dcache.pool.nearline.NearlineStorageHandler$StoreKillCommand.call(NearlineStorageHandler.java:1522)

modules/dcache/src/main/java/org/dcache/pool/nearline/NearlineStorageHandler.java
@@ -599,12 +599,12 @@ public class NearlineStorageHandler
         {
             State oldState = state.getAndSet(State.CANCELED);
             if ( oldState != State.CANCELED) {
-                storage.cancel(uuid);
                 if (oldState == State.ACTIVE) {
                     incCancelFromActive();
                 } else {
                     incCancelFromQueued();
                 }
+                storage.cancel(uuid);
                 synchronized(this) {
                     for (Future<?> task : asyncTasks) {
                         /*