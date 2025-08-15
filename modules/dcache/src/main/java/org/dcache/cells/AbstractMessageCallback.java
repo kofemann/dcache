07b82c3ac89780275faf86ab0dbc5a917d91e468
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2025-08-15 14:33:46 +0200

    core: update AbstractMessageCallback to be CompletableFuture friendly
    Motivation:
    As a part of reducing dcache core less dependable on Guava in ins
    cross-component API we need to provide an alternative for message
    delivery callback.
    
    Modification:
    Update AbstractMessageCallback to implement BiConsumer<T, E> which can
    be combined with CompletableFuture#whenComplete
    
    Result:
    same functionality, less dependency on guava
    
    Acked-by:
    Target: master
    Require-book: no
    Require-notes: no

--- modules/dcache/src/main/java/org/dcache/cells/AbstractMessageCallback.java
+++ modules/dcache/src/main/java/org/dcache/cells/AbstractMessageCallback.java
@@ -1,14 +1,39 @@
 package org.dcache.cells;
 
 import diskCacheV111.util.CacheException;
+import diskCacheV111.util.TimeoutCacheException;
 import diskCacheV111.vehicles.Message;
 import dmg.cells.nucleus.CellPath;
+import dmg.cells.nucleus.NoRouteToCellException;
+import java.util.function.BiConsumer;
 
 
 /**
  * Abstract base class for MessageCallback implementations.
+ *
+ * Example usage:
+ *
+ *
+ * <pre>
+ *     // with Java8 CompletableFuture
+ *      var message = new Message();
+ *      var callback = new MyMessageCallback();
+ *      cellStub.send(cellPath, message, message.getClass(), timeout)
+ *                 .whenCompleteAsync(callback, executor);
+ * </pre>
+ *
+ * or
+ *
+ *  <pre>
+ *      // with Guava ListenableFuture
+ *      var message = new Message();
+ *      var callback = new MyMessageCallback();
+ *      var listenableFuture = cellStub.send(cellPath, message, timeout);
+ *      CellStub.addCallback(listenableFuture, callback, executor);
+ *  </pre>
+ *
  */
-public abstract class AbstractMessageCallback<T extends Message> implements MessageCallback<T> {
+public abstract class AbstractMessageCallback<T extends Message> implements MessageCallback<T>, BiConsumer<T, Throwable> {
 
     private T _reply;
 
@@ -36,5 +61,29 @@ public abstract class AbstractMessageCallback<T extends Message> implements Mess
     @Override
     public void timeout(String message) {
         failure(CacheException.TIMEOUT, message);
+    }
+
+    @Override
+    public void accept(T t, Throwable e) {
+        if (e == null) {
+            setReply(t);
+            if (t.getReturnCode() == 0) {
+                success(t);
+            } else {
+                failure(t.getReturnCode(), t.getErrorObject());
+            }
+        } else {
+            var cause = e.getCause();
+            if (cause instanceof TimeoutCacheException) {
+                timeout(cause.getMessage());
+            } else if (cause instanceof CacheException) {
+                CacheException cacheException = (CacheException) cause;
+                failure(cacheException.getRc(), cacheException.getMessage());
+            } else if (cause instanceof NoRouteToCellException) {
+                noroute(((NoRouteToCellException) cause).getDestinationPath());
+            } else {
+                failure(CacheException.UNEXPECTED_SYSTEM_EXCEPTION, cause);
+            }
+        }
     }
 }
